bd2c6739db70ca0eee950d09f0efc7d03fe134e6
Nejc Haberman <nejc.haberman@gmail.com>
2016-04-29 12:13:43 +0100

    Mapping CLIP to a genome script added

---
+++ Genome_mapping_of_PTBP1_U2AF65_iCLIP/mapping_to_genome-PTBP1_U2AF65_CLIP-pipeline.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -l
+#$ -l h_vmem=16G
+#$ -l tmem=16G
+#$ -l h_rt=32:0:0
+#$ -j y
+#$ -S /bin/bash
+
+PYTHONPATH=/home/programs/Python-2.7.5/bin
+PERL5LIB=/home/programs/ActivePerl-5.16.3.1603-x86_64-linux-glibc-2.3.5-296746/perl/bin
+export PATH=$PYTHONPATH:$PATH
+export PATH=$PERL5LIB:$PATH
+export PATH=/home/programs/tophat-2.0.9.Linux_x86_64:$PATH
+export PATH=/home/programs/bowtie2-2.1.0:$PATH
+export PATH=/home/programs/samtools-0.1.19:$PATH
+export PATH=/home/programs/fastx_toolkit0.0.13:$PATH
+export PATH=/home/programs/bedtools-2.17.0/bin:$PATH
+
+data=$1
+path=`pwd -P`
+
+gunzip ${path}${data}.gz
+
+# clip the adapter and discard clipped sequences and discard the sequences that are shorter then 17 nt; complete cDNAs contains the adapter sequence and incomplete does not
+fastx_clipper -Q 33 -a AGATCGGAAG -C -n -l 17 -i  ${path}${data} -o  ${path}${data}-incomplete.fq
+fastx_clipper -Q 33 -a AGATCGGAAG -c -n -l 17 -i  ${path}${data} -o  ${path}${data}-complete.fq
+
+# fastq to fasta
+fastq_to_fasta -Q 33 -n -i ${path}${data}-incomplete.fq -o ${path}${data}-incomplete.fa
+fastq_to_fasta -Q 33 -n -i ${path}${data}-complete.fq -o ${path}${data}-complete.fa
+rm ${path}${data}-incomplete.fq
+rm ${path}${data}-complete.fq
+
+# map to hg19
+bowtie2-align -x ~/bowtie-indexes/hg19/hg19 -f ${path}${data}-incomplete.fa -S ${path}${data}-incomplete.sam
+bowtie2-align -x ~/bowtie-indexes/hg19/hg19 -f ${path}${data}-complete.fa -S ${path}${data}-complete.sam
+rm ${path}${data}-incomplete-barcodes.fa
+rm ${path}${data}-complete-barcodes.fa
+
+# filter reads with more then 2 mismatches
+samtools view -Sh ${path}${data}-incomplete.sam | grep -e "^@" -e "XM:i:[012][^0-9]" > ${path}${data}-incomplete-2mis.sam
+samtools view -Sh ${path}${data}-complete.sam | grep -e "^@" -e "XM:i:[012][^0-9]" > ${path}${data}-complete-2mis.sam
+
+# SAM to BAM
+samtools view -hSb ${path}${data}-incomplete-2mis.sam > ${path}${data}-incomplete.bam
+samtools view -hSb ${path}${data}-complete-2mis.sam > ${path}${data}-complete.bam
+rm ${path}${data}-incomplete-2mis.sam
+rm ${path}${data}-complete-2mis.sam
+
+# convert bam to bed
+bedtools bamtobed -i ${path}${data}-incomplete.bam > ${path}${data}-incomplete.bed
+bedtools bamtobed -i ${path}${data}-complete.bam > ${path}${data}-complete.bed
+
+# remove duplicates
+cat ${path}${data}-incomplete.bed | sort -k1,1 -k2,2n -k4,4 | uniq > ${path}${data}-incomplete-uniq.bed
+cat ${path}${data}-complete.bed | sort -k1,1 -k2,2n -k4,4 | uniq > ${path}${data}-complete-uniq.bed
+rm ${path}${data}-incomplete.bed
+rm ${path}${data}-complete.bed
+
+# compress the original data
+gzip ${path}${data}
+
